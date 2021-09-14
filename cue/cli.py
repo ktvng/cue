37985b809837b7c717179bddbe316d677e5db60d
ktvng <35818433+ktvng@users.noreply.github.com>
2021-09-13 18:59:57 -0700

    change getopt to argparse

--- cue/cli.py
+++ cue/cli.py
@@ -1,63 +1,62 @@
-import getopt
-import sys
+import argparse
 from cue.orchestrator import Orchestrator 
 
 def run():
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser(description="Load and run an automated data analysis pipeline")
+    parser.add_argument('-i', '--input', 
+        nargs='?',   
+        type=str, 
+        required=True,
+        help='input path to a pipeline file')
 
-def main(argv):
-    # -r <root> -i [pipeline] -c 
-    try:
-        opts, args = getopt.getopt(argv, "i:cn:w:p:f:d:vh", 
-            [
-                'input=', 
-                'n_times_before_timeout=', 
-                'wait_time_between_tries=', 
-                'max_processes=', 
-                'from=', 
-                'clean', 
-                'dir=', 
-                'verbose',
-                'help'
-            ])
+    parser.add_argument('-n', '--n_times_before_timeout',
+        nargs='?', 
+        type=int, 
+        help='number of times before timeout')
 
-    except:
-        exit(2)
+    parser.add_argument('-w', '--waits',
+        nargs='?',
+        type=float,
+        help='time to wait between retries')
 
-    options = {}
-    path_to_pipeline_json = None
-    purge = False
-    tmp_dir = None
-    verbose = False
+    parser.add_argument('-p', '--cpus',
+        nargs='?',
+        type=int,
+        help='number of processors to use')
 
-    for opt, arg in opts:
-        if opt in ['-i', '--input'] :
-            path_to_pipeline_json = arg
-        elif opt in ['-n', '--n_times_before_timeout']:
-            options['n_times_before_timeout'] = int(arg)
-        elif opt in ['-w', '--wait_time_between_tries']:
-            options['wait_time_between_tries'] = float(arg)
-        elif opt in ['-p', '--max_processes']:
-            options['max_processes'] = int(arg)
-        elif opt in ['-f', '--from']:
-            options['from'] = int(arg)
-        elif opt in ['--clean']:
-            purge = True
-        elif opt in ['-d', '--dir']:
-            tmp_dir = arg + "/"
-        elif opt in ['-v', '--verbose']:
-            verbose = True
-        elif opt in ['-h', '--help']:
-            # TODO: implement
-            exit(0)
+    parser.add_argument('-f', '--from', 
+        nargs='?',
+        type=int,
+        help='block serial number to being pipeline at')
 
-    if path_to_pipeline_json is None:
-        print("exception: expected json filename with [-i]")
-        exit(2)
+    parser.add_argument('--clean',
+        action='store_true',
+        default=False,
+        help='cleans cache and removes all temporary files/directories')
 
-    if purge:
-        Orchestrator(using_directory=tmp_dir, verbose=verbose).purge()
+    parser.add_argument('-v', '--verbose',
+        action='store_true',
+        default=False,
+        help='run verbose with status messages printed')
+
+    parser.add_argument('-d', '--dir',
+        nargs=1,
+        type=str,
+        help='use named path instead of default for temporary directory')
+
+    args = parser.parse_args()
+    vargs = vars(args)
+    is_verbose = vargs['verbose']
+    should_purge = vargs['clean']
+    path_to_pipeline_json = vargs['input']
+    tmp_dir = vargs['dir']
+
+    options = { k : v for k, v in vargs.items() if v is not None }
+
+    if should_purge:
+        Orchestrator(using_directory=tmp_dir, verbose=is_verbose).purge()
         exit(0)
 
-    so = Orchestrator(using_directory=tmp_dir, verbose=verbose).read(yaml_file=path_to_pipeline_json).queue_tasks().run(given=options)
+    so = Orchestrator(using_directory=tmp_dir, verbose=is_verbose).read(yaml_file=path_to_pipeline_json).queue_tasks().run(given=options)
     so.clean()
+    print(vars(args))
